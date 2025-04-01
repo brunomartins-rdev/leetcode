bc6b7e1c4e5bd12c5a154790961325babd05ce96
brunomartins-rdev <bruno.silva.martins@hotmail.com>
2025-04-02 00:32:29 +0100

    #242

---
+++ easy/242_validAnagram.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        d_s = {}
+        d_t = {}
+
+        for letter in s:
+            if letter in d_s:
+                d_s[letter] += 1
+            else:
+                d_s[letter] = 1
+
+        for letter in t:
+            if letter in d_t:
+                d_t[letter] += 1
+            else:
+                d_t[letter] = 1
+
+        return d_s == d_t
+
+solution = Solution()
+print(solution.isAnagram('anagram', 'anagarm'))
+print(solution.isAnagram('anaggram', 'anagarm'))
+
+# Optimal solution would be just
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         a = sorted(s)
+#         b = sorted(t)
+#         return a == b
