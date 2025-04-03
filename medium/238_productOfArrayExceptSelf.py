0bd2b71d8a7372d93fa930d1d30268b10e7f4179
brunomartins-rdev <bruno.silva.martins@hotmail.com>
2025-04-03 11:29:12 +0100

    #238

--- medium/238_productOfArrayExceptSelf.py
+++ medium/238_productOfArrayExceptSelf.py
@@ -10,7 +10,7 @@ class Solution:
             res[i] = pre
             pre *= nums[i]
 
-        for i in range(len(nums), -1, -1):
+        for i in range(len(nums) - 1, -1, -1):
             res[i] *= pos
             pos *= nums[i]
 
